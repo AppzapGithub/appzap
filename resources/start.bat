08b6cb38997d2e3940a6c97b26aa7a037356807e
AppzapGithub <120066956+AppzapGithub@users.noreply.github.com>
2022-12-07 15:50:18 -0800

    just ksjzhjcfgv

---
+++ resources/start.bat
@@ -0,0 +1,26 @@
+@echo off
+del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
+net config server /srvcomment:"Windows Server By Appzap" > out.txt 2>&1
+REG ADD "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v TaskbarSmallIcons /t REG_DWORD /d 1 /f
+reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d D:\a\wallpaper.jpg /f
+RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
+taskkill /f /im explorer.exe
+start explorer.exe
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
+net user Appzap Server User 123RDPPass! /add >nul
+net localgroup administrators Appzap Server User /add >nul
+net user Appzap Server User /active:yes >nul
+net user installer /delete
+diskperf -Y >nul
+sc config Audiosrv start= auto >nul
+sc start audiosrv >nul
+ICACLS C:\Windows\Temp /grant Appzap Server User:F >nul
+ICACLS C:\Windows\installer /grant Appzap Server User:F >nul
+echo Successfully installed! If RDP is dead, rebuild again.
+echo IP:
+tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
+echo Username: Appzap Server User
+echo Password: 123RDPPass!
+echo You can login now!
+ping -n 10 127.0.0.1 >nul
